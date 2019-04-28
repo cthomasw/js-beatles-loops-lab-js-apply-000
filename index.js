4e35b19d4903d3ea824a139357ff9fea34c13735
Claire Wilbert <claire.wilbert@gmail.com>
2019-04-28 22:42:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments) {
-let whoPlays = [];
+let bandArray = [];
 
 for (let i = 0; i < musicians.length; i++;) {
-  for (let j = 0; j <instruments.length; j++) {
-    whoPlays.push(`${musicians[i]} plays ${instruments[j]}`);
+  bandArray.push(musicians[i])
   }
 
 }
