cd840b76a776d77b6bdf22727f0dc82e0c279d2f
Claire Wilbert <claire.wilbert@gmail.com>
2019-04-28 23:07:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 
 function theBeatlesPlay(musicians, instruments) {
   let allMembers = [];
-
   for (let musicians i = 0; i < musicians.length; i++) {
     for (let instruments j = 0; j < instruments.length; j++) {
   allMembers.push(musicians[i] + ' plays ' + instruments[i])
   }
+}
+
 }
