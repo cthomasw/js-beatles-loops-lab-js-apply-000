63d540af75d32229a659f2efe7ecb1ccd51188be
Claire Wilbert <claire.wilbert@gmail.com>
2019-04-28 22:49:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 // add solution here
-const theBeatlesPlay = (bandArray) => {
-let bandArray = [];
-for (let i = 0; i < musicians.length; i++;) {
-  for (let j = 0; j < instruments.length; j++) {
-  bandArray.push(`${musicians[i]} plays ${instruments[j]}`)
-    }
+function theBeatlesPlay(musicians, instruments) {
+  let array = [];
+
+  for (i = 0; i < musicians.length; i++) {
+    array.push(musicians[i] + ' plays ' + instruments[i])
   }
 }
