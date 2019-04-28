40bca7df3f763f6aa0e2915a97944c3561edd4f7
Claire Wilbert <claire.wilbert@gmail.com>
2019-04-28 23:09:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
-const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 
-function theBeatlesPlay(musicians, instruments) {
-  let allMembers = [];
-  for (let musicians i = 0; i < musicians.length; i++) {
-    for (let instruments j = 0; j < instruments.length; j++) {
-  allMembers.push(musicians[i] + ' plays ' + instruments[i])
-  }
-}
-return allMembers[];
-}
