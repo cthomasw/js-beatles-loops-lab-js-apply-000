c4fd35e7e7732cfcdb4b84f83fc89eaaf3e24e66
Claire Wilbert <claire.wilbert@gmail.com>
2019-04-28 22:38:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ function theBeatlesPlay(musicians, instruments) {
 let whoPlays = [];
 
 for (let i = 0; i < musicians.length; i++;) {
-
-
+  for (let j = 0; j <instruments.length; j++) {
     whoPlays.push(`${musicians[i]} plays ${instruments[j]}`);
   }
 
