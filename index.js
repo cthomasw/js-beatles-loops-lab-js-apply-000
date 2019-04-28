efb17f2cf3f91a7b5d06004117bf861cd5b683c7
Claire Wilbert <claire.wilbert@gmail.com>
2019-04-28 23:01:10 +0000

    Automatically backed up by Learn

--- index.js
+++
@@ -1,9 +0,0 @@
-// add solution here
-
-function theBeatlesPlay(musicians, instruments) {
-  const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
-  const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
-  let beatlesArray = [];
-  for (i = 0; i < musicians.length; i++) {
-    beatlesArray.push(`${musicians[i]} plays ${instruments[i]}`);
-  }
