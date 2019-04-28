e84453eb1e7a3ce379bb43a60e550b7bc36414c2
Claire Wilbert <claire.wilbert@gmail.com>
2019-04-28 22:58:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 // add solution here
-const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
-const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 
 function theBeatlesPlay(musicians, instruments) {
+  const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+  const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
   let beatlesArray = [];
-
   for (i = 0; i < musicians.length; i++) {
     beatlesArray.push(musicians[i]);
+  }
+  for (j = 0; j <instruments.length; j++) {
+
   }
 }
