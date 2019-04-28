20718d32eb54eb6b4cee6fa4d8de4b370825a736
Claire Wilbert <claire.wilbert@gmail.com>
2019-04-28 23:04:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function theBeatlesPlay(musicians, instruments) {
   let allMembers = [];
   for (let allMembers i = 0; i < allMembers.length; i++) {
-    
+  allMembers.push(musicians[i] + ' plays ' + instruments[i])
   }
+  return allMembers;
 }
